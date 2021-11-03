b2c7b8733b183943c3f66be49cf39aa0305bc3c2
AdrianMEvans <73572438+AdrianMEvans@users.noreply.github.com>
2021-11-03 15:08:34 +0800

    changed receiving email

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'adrian@polywhiz.com';
+  $receiving_email_address = 'adrian@rhinolinings.co.za';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  // $contact->smtp = array(
-  //   'host' => 'srv48.hostserv.co.za',
-  //   'username' => 'adrian@rhinolinings.co.za',
-  //   'password' => 'Y3in@rhinolinings',
-  //   'port' => '587'
-  // );
+  $contact->smtp = array(
+    'host' => 'srv48.hostserv.co.za',
+    'username' => 'adrian@rhinolinings.co.za',
+    'password' => 'Y3in@rhinolinings',
+    'port' => '587'
+  );
   
 
   $contact->add_message( $_POST['name'], 'From');
