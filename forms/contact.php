c79f745bd5bd7b37c255829de1ccee42be0921f2
AdrianMEvans <73572438+AdrianMEvans@users.noreply.github.com>
2021-11-03 10:19:03 +0800

    removed SMTP from php

--- forms/contact.php
+++ forms/contact.php
@@ -25,12 +25,12 @@
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
   
-  $contact->smtp = array(
-    'host' => 'srv48.hostserv.co.za',
-    'username' => 'adrian@rhinolinings.co.za',
-    'password' => 'Y3in@rhinolinings',
-    'port' => '587'
-  );
+  // $contact->smtp = array(
+  //   'host' => 'srv48.hostserv.co.za',
+  //   'username' => 'adrian@rhinolinings.co.za',
+  //   'password' => 'Y3in@rhinolinings',
+  //   'port' => '587'
+  // );
   
 
   $contact->add_message( $_POST['name'], 'From');
