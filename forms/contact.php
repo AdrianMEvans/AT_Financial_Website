d0f162ea7e451e83e7e9a9502171cdf45c55ab87
AdrianMEvans <73572438+AdrianMEvans@users.noreply.github.com>
2022-02-19 15:22:48 +0800

    changed subject

--- forms/contact.php
+++ forms/contact.php
@@ -21,7 +21,7 @@
   $contact->to = $receiving_email_address;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
-  $contact->subject = 'Form Submission from AT Financial' + $_POST['subject'];
+  $contact->subject = $_POST['subject'];
 
 
   
